2a85e11b5379e71177e3bf4f54750a2de2d43be1
Liye Ng <liye1998@hotmail.com>
2023-04-01 20:24:41 +0800

    Updated register function in RegisterActivity

--- app/src/main/java/com/example/project/RegisterActivity.kt
+++ app/src/main/java/com/example/project/RegisterActivity.kt
@@ -4,17 +4,23 @@ import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONObject
 import java.io.IOException
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
     }
 
     fun register(view: View) {
@@ -24,26 +30,32 @@ class RegisterActivity : AppCompatActivity() {
             createToast("Please ensure your confirmed password is the same as your password!")
 
         } else {
+            val email = findViewById<EditText>(R.id.editTextEmailRegister).text.toString()
+            val password = findViewById<EditText>(R.id.editTextPasswordRegister).text.toString()
             val client = OkHttpClient()
 
-            val requestBody: RequestBody = FormBody.Builder()
-                .add("email", findViewById<EditText>(R.id.editTextEmailRegister).text.toString())
-                .add("password", findViewById<EditText>(R.id.editTextPasswordRegister).text.toString())
-                .build()
+            val jsonObject = JSONObject()
+            jsonObject.put("email", email)
+            jsonObject.put("password", password)
+            val json = jsonObject.toString()
 
 
             val request = Request.Builder()
-                .url("http://127.0.0.1/register")
-                .post(requestBody)
+                .url("http://10.0.2.2:5000/register")
+                .post(json.toRequestBody("application/json".toMediaTypeOrNull()))
                 .build()
 
+            println(request.toString())
+
             client.newCall(request).enqueue(object : Callback {
-                override fun onFailure(call: okhttp3.Call, e: IOException) {
+                override fun onFailure(call: Call, e: IOException) {
+                    println(e)
                     createToast("There has been an error. Please try again!")
 
                 }
 
-                override fun onResponse(call: okhttp3.Call, response: Response) {
+                override fun onResponse(call: Call, response: Response) {
+                    println(response)
                     if (response.code == 400) {
                         createToast("User has already been created with this email. Please use another one!")
                     } else {
@@ -57,13 +69,17 @@ class RegisterActivity : AppCompatActivity() {
     }
 
     private fun createToast(message: String) {
-        val toast = Toast.makeText(this@RegisterActivity,
-            message,
-            Toast.LENGTH_LONG)
-        val toastView = toast.view
-        toastView?.setBackgroundColor(Color.RED)
-        val text = toastView?.findViewById(android.R.id.message) as TextView
-        text.setTextColor(Color.WHITE)
-        toast.show()
+        val handler = Handler(Looper.getMainLooper())
+        handler.post {
+            val toast = Toast.makeText(this@RegisterActivity,
+                message,
+                Toast.LENGTH_LONG)
+            val toastView = toast.view
+            toastView?.setBackgroundColor(Color.RED)
+            val text = toastView?.findViewById<TextView>(android.R.id.message)
+            text?.setTextColor(Color.WHITE)
+            toast.show()
+        }
+
     }
 }
